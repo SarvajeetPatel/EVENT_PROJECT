012b9f9952edf6f905e0b2013ff0a36d4274e503
SarvajeetPatel <sarvajeet@zealousweb.com>
2024-03-21 11:53:36 +0530

    first

--- frontpages/src/App.js
+++ frontpages/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import SignInSide from './components/SignIn';
+import SignInSide from './pages/SignIn';
 
 function App() {
   return (
